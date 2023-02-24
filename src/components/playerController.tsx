50108697fb2e8b56a3d16b84cc5cb67d2b508b3f
AbhishekOjha7 <abhishek.ojha@appinventiv.com>
2023-02-24 22:23:19 +0530

    video implement

--- src/components/playerController.tsx
+++ src/components/playerController.tsx
@@ -6,22 +6,43 @@ import localimages from '../utils/localimages';
 const PlayerController = (props: any) => {
   const {playing, onPlay, onPause, skipForwards, skipBackwards} = props;
   return (
-    <View style={{}}>
+    <View
+      style={{
+        flexDirection: 'row',
+        marginHorizontal: 40,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        height: 100,
+      }}>
+      <TouchableOpacity style={{}} onPress={skipBackwards}>
+        <Image
+          resizeMode="contain"
+          style={{height: 35, width: 35, tintColor: 'white'}}
+          source={localimages.backward}
+        />
+      </TouchableOpacity>
+
       <TouchableOpacity style={{}} onPress={playing ? onPause : onPlay}>
         {playing ? (
-          // <VideoPause height="50" width="50" />
           <Image
             resizeMode="contain"
-            style={{height: 40, width: 45, tintColor: 'white'}}
+            style={{height: 30, width: 35, tintColor: 'white'}}
             source={localimages.pauseIcon}
           />
         ) : (
           <Image
             resizeMode="contain"
-            style={{height: 40, width: 45, tintColor: 'white'}}
+            style={{height: 30, width: 35, tintColor: 'white'}}
             source={localimages.playIcon}
           />
         )}
+      </TouchableOpacity>
+      <TouchableOpacity style={{}} onPress={skipForwards}>
+        <Image
+          resizeMode="contain"
+          style={{height: 35, width: 35, tintColor: 'white'}}
+          source={localimages.forward}
+        />
       </TouchableOpacity>
     </View>
   );
--- src/components/videoPlayerComponent.tsx
+++ src/components/videoPlayerComponent.tsx
@@ -4,10 +4,13 @@ import Video from 'react-native-video';
 import {normalize} from '../utils/dimensions';
 import localimages from '../utils/localimages';
 import PlayerController from './playerController';
+import SeekBar from './seekBarComponent';
+const videoRef = React.createRef<any>();
 const VideoPlayerComponent = ({videoUrl}: any) => {
   const [play, setPlay] = useState(false);
   const [showControl, setShowControl] = useState(true);
-  console.log('videoUrl', videoUrl);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   const handlePlay = () => {
     setTimeout(() => setShowControl(false), 500);
@@ -24,19 +27,79 @@ const VideoPlayerComponent = ({videoUrl}: any) => {
     setPlay(true);
   };
 
+  const skipBackward = () => {
+    videoRef?.current?.seek(currentTime - 10);
+    setCurrentTime(currentTime - 10);
+  };
+
+  const skipForward = () => {
+    console.log(currentTime + 10, currentTime);
+    videoRef?.current?.seek(currentTime + 10);
+    setCurrentTime(currentTime + 10);
+  };
+
+  const onSeek = (data: any) => {
+    videoRef?.current?.seek(data.seekTime);
+    setCurrentTime(data.seekTime);
+  };
+  const onProgress = (data: any) => {
+    setCurrentTime(data.currentTime);
+  };
+
+  // console.log(currentTime);
+
   return (
     <View style={styles.container}>
       <Video
+        ref={videoRef}
         source={{uri: videoUrl}}
         style={styles.video}
         controls={false}
         paused={!play}
+        onProgress={onProgress}
       />
+
       <PlayerController
         onPlay={handlePlay}
         onPause={handlePlayPause}
         playing={play}
+        skipBackwards={skipBackward}
+        skipForwards={skipForward}
       />
+
+      <TouchableOpacity style={{position: 'absolute', top: 10, left: 10}}>
+        <Image
+          style={{
+            height: 30,
+            width: 30,
+            tintColor: 'white',
+          }}
+          source={localimages.left_arrow}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          bottom: 10,
+          right: 10,
+        }}>
+        <Image
+          resizeMode="contain"
+          style={{
+            height: 30,
+            width: 30,
+            tintColor: 'white',
+          }}
+          source={localimages.fullscreen}
+        />
+      </TouchableOpacity>
+      {/* <SeekBar
+        currentTime={currentTime}
+        duration={duration > 0 ? duration : 0}
+        onSlideStart={handlePlayPause}
+        onSlideComplete={handlePlayPause}
+        onSlideCapture={onSeek}
+      /> */}
     </View>
   );
 };
@@ -48,7 +111,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000',
     justifyContent: 'center',
-    alignItems: 'center',
+    // alignItems: 'center',
   },
   video: {
     // flex: 1,
